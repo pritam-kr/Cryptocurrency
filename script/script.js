3e8aaf2787154a39422126974d79ac843afc892a
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2021-09-01 19:28:47 +0530

    almost done

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=1&page=1&sparkline=false'
+const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=10&page=1&sparkline=false'
 
 window.addEventListener('load', (e) =>{
     fetch(url)
